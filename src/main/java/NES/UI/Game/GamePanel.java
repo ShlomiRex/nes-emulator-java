6a1b0a518a3ba84b25a5d1fc20a7d9d0c9d164c0
Shlomi <vgtvgy1@gmail.com>
2023-06-24 04:01:52 +0300

    working drawing frame to gray

--- src/main/java/NES/UI/Game/GamePanel.java
+++ src/main/java/NES/UI/Game/GamePanel.java
@@ -14,34 +14,30 @@ public class GamePanel extends JPanel {
     private static final int TILE_COLUMNS = 32;
     private static final int TILE_ROWS = 30;
 
-    private static final int PIXEL_SIZE = 4;
+    //private static final int PIXEL_SIZE = 4;
     private static final int PIXEL_COLUMNS = 256;
     private static final int PIXEL_ROWS = 240;
 
     private byte[] frameBuffer;
 
     public GamePanel() {
-        setBackground(Color.BLACK);
+        int scale = 2;
+        setPreferredSize(new Dimension(PIXEL_COLUMNS * scale, PIXEL_ROWS * scale));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        logger.debug("Painting component");
+        logger.debug("Width: {}, Height: {}", getWidth(), getHeight());
 
         if (frameBuffer == null)
             return;
 
-        logger.debug("Frame buffer is null");
-
-        int tileWidth = getWidth() / TILE_COLUMNS;
-        int tileHeight = getHeight() / TILE_ROWS;
-        int pixelSize = Math.min(getWidth() / PIXEL_COLUMNS, getHeight() / PIXEL_ROWS);
-
-        // Clear the screen with a background color
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, PIXEL_COLUMNS, PIXEL_ROWS);
+        int pixel_width = getWidth() / PIXEL_COLUMNS;
+        int pixel_height = getHeight() / PIXEL_ROWS;
+        int tile_width = getWidth() / TILE_COLUMNS;
+        int tile_height = getHeight() / TILE_ROWS;
 
         // Draw the frame buffer
         for (int y = 0; y < PIXEL_ROWS; y++) {
@@ -52,27 +48,17 @@ public class GamePanel extends JPanel {
 
                 // Draw the pixel with the appropriate color
                 g.setColor(pixelColor);
-                g.fillRect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+                g.fillRect(x * pixel_width, y * pixel_height, pixel_width, pixel_height);
             }
         }
 
-        // Draw tile grid
-        g.setColor(Color.BLUE);
-        for (int row = 0; row < TILE_ROWS; row++) {
-            for (int col = 0; col < TILE_COLUMNS; col++) {
-                int x = col * TILE_SIZE;
-                int y = row * TILE_SIZE;
-                g.drawRect(x, y, TILE_SIZE, TILE_SIZE);
-            }
-        }
-
-        // Draw pixel grid
-//        g.setColor(Color.BLACK);
-//        for (int row = 0; row < PIXEL_ROWS; row++) {
-//            for (int col = 0; col < PIXEL_COLUMNS; col++) {
-//                int x = col * PIXEL_SIZE;
-//                int y = row * PIXEL_SIZE;
-//                g.drawRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+//        // Draw tile grid
+//        g.setColor(Color.BLUE);
+//        for (int row = 0; row < TILE_ROWS; row++) {
+//            for (int col = 0; col < TILE_COLUMNS; col++) {
+//                int x = col * tile_width;
+//                int y = row * tile_height;
+//                g.drawRect(x, y, tile_width, tile_height);
 //            }
 //        }
     }
--- src/main/java/NES/UI/Game/GameWindow.java
+++ src/main/java/NES/UI/Game/GameWindow.java
@@ -1,15 +1,16 @@
 package NES.UI.Game;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow extends JFrame {
     public GameWindow(GamePanel panel) {
         add(panel);
 
-        setSize(800, 600);
-        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("NES Emulator - Written by: Shlomi Domnenko");
         setVisible(true);
+        pack();
+        setLocationRelativeTo(null);
     }
 }
