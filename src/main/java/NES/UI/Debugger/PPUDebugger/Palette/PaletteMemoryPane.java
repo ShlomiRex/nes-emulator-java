cfed89814fb9b66e562be8574adb4a4aada20a03
Shlomi <vgtvgy1@gmail.com>
2023-07-04 16:26:42 +0300

    When hover over palette memory display the mem location in title border

--- src/main/java/NES/UI/Debugger/PPUDebugger/Palette/PaletteMemoryPane.java
+++ src/main/java/NES/UI/Debugger/PPUDebugger/Palette/PaletteMemoryPane.java
@@ -1,17 +1,40 @@
 package NES.UI.Debugger.PPUDebugger.Palette;
 
+import NES.Common;
+
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PaletteMemoryPane extends JPanel {
     public PaletteMemoryPane() {
-        setBorder(BorderFactory.createTitledBorder("Palette Memory"));
+        TitledBorder titledBorder = BorderFactory.createTitledBorder("Palette Memory");
+        setBorder(titledBorder);
 
         GridBagLayout layout = new GridBagLayout();
         setLayout(layout);
 
         for (int i = 0; i < 32; i++) {
             JPanel tile = new PaletteTilePane(0, 24, 32);
+            int finalI = i;
+            tile.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    super.mouseEntered(e); // Call PaletteTilePane mouseEntered (don't override it)
+                    short palette_memory_location = (short) (0x3F00 + finalI);
+                    titledBorder.setTitle("Palette Memory: " + Common.shortToHex(palette_memory_location, true));
+                    repaint();
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    super.mouseExited(e);
+                    titledBorder.setTitle("Palette Memory");
+                    repaint();
+                }
+            });
 
             int col = i % 16;
             int row = i / 16;
